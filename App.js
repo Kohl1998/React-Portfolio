5eed508f24700ec88d0db2da1ab0308443aa81e1
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-03-04 12:18:12 +0000

    added routes for navigation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './src/components/Header';
 import Home from './src/components/Home';
 import Gallery from './src/components/Gallery';
@@ -7,7 +7,17 @@ import Contact from './src/components/Contact';
 
 
 function App () {
-    
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path='header' element={<Header/>}/>
+                <Route path='home' element={<Home/>}/>
+                <Route path='gallery' element={<Gallery/>}/>
+                <Route path='project' element={<Project/>}/>
+                <Route path='contact' element={<Contact/>}/>
+            </Routes>
+        </HashRouter>
+    )
 }
 
 export default App

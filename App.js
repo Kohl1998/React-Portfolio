cad9a3ad3c7d588366ac3a89bdc39d7bdb2aa6f6
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-03-04 12:06:40 +0000

    added template function and import links to all files

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './src/components/Header';
+import Home from './src/components/Home';
+import Gallery from './src/components/Gallery';
+import Contact from './src/components/Contact';
+
+
+function App () {
+    
+}
+
+export default App
---
+++ src/components/Gallery.js
@@ -0,0 +1,6 @@
+function Gallery () {
+
+}
+
+
+export default Gallery

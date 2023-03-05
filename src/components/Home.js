2af2673af0848f81945903bd1b42d74acd4f1b7c
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-03-05 14:37:31 +0000

    added bootstrap navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
+// Simple navbar with different sections to web pages
 function Home () {
-
+    return (
+        <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Projects</Nav.Link>
+            <Nav.Link href="#pricing">Gallery</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    )
 }
 
 

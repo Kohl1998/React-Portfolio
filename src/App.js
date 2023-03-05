fa7ed607ad76dc1195aece2f5b0c68591ba578e9
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-03-05 09:37:46 +0000

    defined project function in route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { HashRouter, HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './src/components/Header';
-import Home from './src/components/Home';
-import Gallery from './src/components/Gallery';
-import Contact from './src/components/Contact';
-
+import Header from './components/Header';
+import Home from './components/Home';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
 
 function App () {
     return (
@@ -14,7 +14,7 @@ function App () {
                 <Route path='/' element={<Home/>}/>
                 <Route path='header' element={<Header/>}/>
                 <Route path='gallery' element={<Gallery/>}/>
-                <Route path='project' element={<Project/>}/>
+                <Route path='project' element={<Projects/>}/>
                 <Route path='contact' element={<Contact/>}/>
             </Routes>
         </HashRouter>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Gallery () {
+function Projects () {
 
 }
 
-export default Gallery;
+export default Projects;

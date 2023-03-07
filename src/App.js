4199839dc4565b4d3ab48fbb9f2186cd88756699
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-03-07 21:29:08 +0000

    removed unneccessary routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
-import Gallery from './components/Gallery';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
 import NavTabs from './components/navLinks';
+import Footer from './components/Footer';
+
 
 function App () {
     return (
@@ -14,10 +15,10 @@ function App () {
                 {/* / Sets home as default path */}
                 <Route exact path='/' element={<Home/>}/>
                 <Route path='header' element={<Header />}/>
-                <Route path='gallery' element={<Gallery />}/>
                 <Route path='projects' element={<Projects />}/>
                 <Route path='contact' element={<Contact />}/>
             </Routes>
+            <Footer />
         </Router>
     )
 }

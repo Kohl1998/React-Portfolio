97904fe6cd4fd814fbcfc1e62880075c6ecbe3b3
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-03-11 19:02:14 +0000

    added contact form to page

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer"
+import "./Contact.css";
+import { useState } from "react";
+
+
+function Contact() {
+    
+const handleChange = (e) => {
+setFormData({
+    ...formData, [e.target.name]: e.target.value
+})
+    }
+
+    const handleSubmit = (e) => {
+        {/* prevents page from submitting information */}
+        e.preventDefault();
+    } 
+    
+    /* state to hold user information */
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    })
+    
+    return (
+        <div>
+            <Header />
+            <h3>Reach out to me using links in the footer!</h3>
+            <div className="form-container">
+            <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Enter your name" name="name" value={formData.name} onChange={handleChange} />
+            <input type="email" placeholder="Enter your email" name="email" value={formData.email} onChange={handleChange} />
+            <textarea type="text" placeholder="Enter your message" name="message" value={formData.message} onChange={handleChange}  cols="10" rows="30" />
+            <button type="submit" className="btn">submit</button>
+            </form>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Contact
